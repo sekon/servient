26f389cde208c8ea891b0a1be6a3de79b5f84bcc
Harish Badrinath <harishbadrinath@gmail.com>
2011-12-28 13:48:23 +0530

    Added Command line parsing code. Needs to be validated though.

--- servient.sh
+++ servient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #################################################################################################
-#Purpose: Automaitcally Test and validate shell script(s) submitted by fosse participants	#
+#Purpose: <To be done>										#
 #Primary Author: Harish Badrinath < harish [at] fossee.in>					#
 #Taken Over date/Creation date: Sun, 06 Nov 2011 21:38:58 +0530					#
 #Taken Over by:											#
@@ -19,6 +19,12 @@ USER_INFO_UID_STRING="User ID"
 REPORT_FILE="report.txt"
 VERBOSE_OUTPUT=0
 ####################################### CONFIG DATA ENDS ########################################
+
+##TODO: Get a list of all variables in a bash script.
+# VARIABLES=$(echo "compgen -A variable" >> "$PLUGIN_FILE" ;source "$PLUGIN_FILE";sed -i '$d' "$PLUGIN_FILE")
+# echo "$VARIABLES" | grep <PAttern>
+# even if $PLUGIN_FILE redefines/rewrites variables .. it wont be seen here even when the script is being sourced.
+
 
 function get_user_id ()
 {
@@ -47,72 +53,168 @@ IS_ROOT=`id | grep -w root  | wc -l`
 
 function show_help_screen ()
 {
-	MY_OPTIONAL_STRING=`echo $OPTION_STRING | sed 's/^:-://' |sed 's/\([a-zA-Z]\)/\ \1/g' |sed 's/\([a-zA-Z]\)/-\1/g' |sed 's/:/\ OPTION\ /g'`
-	echo "Usage for $0 are $MY_OPTIONAL_STRING" "--verbose[=VALUE] --help"
+	MY_OPTIONAL_STRING=`echo $SERVIENT_OPTION_STRING | sed 's/^:-://' |sed 's/\([a-zA-Z]\)/\ \1/g' |sed 's/\([a-zA-Z]\)/-\1/g' |sed 's/:/\ OPTION\ /g'`
+	echo "Available options for $0 are $MY_OPTIONAL_STRING" "--verbose[=VALUE] --help"
 }
 
 
 #Special thanks to http://wiki.bash-hackers.org/howto/getopts_tutorial, for the awesome tutorial.
 # and http://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options/7680682#7680682
-OPTION_STRING=":-:d:Df:hm:r:R:u:"
+SERVIENT_OPTION_STRING=":-:d:Df:hm:r:R:s:u:"
 
+SERVIENT_verbose_is_set=0
+SERVIENT_delay_is_set=0
+SERVIENT_debug_is_set=0
+SERVIENT_uinfo_file_is_set=0
+SERVIENT_uinfo_string_is_set=0
+SERVIENT_meta_dir_is_set=0
+SERVIENT_ref_dir_is_set=0
+SERVIENT_result_file_is_set=0
+SERVIENT_sol_dir_is_set=0
+SERVIENT_EXIT_ERROR_SCRIPT_CONFIG=200
 ## TODO check for multiple arguments and valid arguments
-while getopts "$OPTION_STRING" opt; do
+while getopts "$SERVIENT_OPTION_STRING" opt; do
 	case $opt in
 		-)
 			case "${OPTARG}" in
 				verbose)
-					#Called only when --verbose is called ..
-					val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-					echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
-					echo "Naked Verbosity" >&2;
+					if (( ! $SERVIENT_verbose_is_set ))
+					then
+						#Called only when --verbose is called ..
+						val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+						echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
+						echo "Naked Verbosity" >&2;
+						SERVIENT_verbose_is_set=1
+					else
+						echo "More than one instance of ${OPTARG} given during invocation" >&2
+						exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+					fi
 					;;
 				verbose=*)
-					val=${OPTARG#*=}
-					opt=${OPTARG%=$val}
-					echo "Parsing option: '--${opt}', value: '${val}'" >&2
+					if (( ! $SERVIENT_verbose_is_set ))
+					then
+						val=${OPTARG#*=}
+						opt=${OPTARG%=$val}
+						echo "Parsing option: '--${opt}', value: '${val}'" >&2
+						SERVIENT_verbose_is_set=1
+					else
+						echo "More than one instance of ${OPTARG} given during invocation" >&2
+						exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+					fi
 					;;
 				help)
-					show_help_screen
+					if [ $# -eq 1 ]
+					then
+						show_help_screen
+					else
+						echo "Option ${OPTARG} needs to be the only argument" >&2
+						exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+					fi
 					;;
 				*)
 					if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
-					echo "Unknown option --${OPTARG}" >&2
+						echo "Unknown option --${OPTARG}" >&2
 					fi
+					exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
 					;;
 				esac
 			;;
 		d)
-			echo "-d was triggered, Delay will be  $OPTARG" >&2
+			if (( ! $SERVIENT_delay_is_set ))
+			then
+				echo "-d was triggered, Delay will be  $OPTARG" >&2
+				SERVIENT_delay_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		D)
-			echo "-D was trigerred, you have enabled bash debugging" >&2
+			if (( ! $SERVIENT_debug_is_set ))
+			then
+				echo "-D was trigerred, you have enabled bash debugging" >&2
+				SERVIENT_debug_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		f)
-			echo "-f was triggered, $OPTARG file in each directory will be queried for user info" >&2
+			if (( ! $SERVIENT_uinfo_file_is_set ))
+			then
+				echo "-f was triggered, $OPTARG file in each directory will be queried for user info" >&2
+				SERVIENT_uinfo_file_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		h)
-			show_help_screen
+			if [ $# -eq 1 ]
+			then
+				show_help_screen
+			else
+				echo "Option ${OPTARG} needs to be the only argument" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		m)
-			echo "-m was triggered, Parameter: $OPTARG is the meta directory" >&2
+			if (( ! $SERVIENT_meta_dir_is_set ))
+			then
+				echo "-m was triggered, Parameter: $OPTARG is the meta directory" >&2
+				SERVIENT_meta_dir_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		r)
-			echo "-r was triggered, Parameter: $OPTARG is the reference directory" >&2
+			if (( ! $SERVIENT_ref_dir_is_set ))
+			then
+				echo "-r was triggered, Parameter: $OPTARG is the reference directory/file" >&2
+				SERVIENT_ref_dir_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		R)
-			echo "-R was triggered, Report will be returned to  $OPTARG" >&2
+			if (( ! $SERVIENT_result_file_is_set ))
+			then
+				echo "-R was triggered, Report will be returned to  $OPTARG" >&2
+				SERVIENT_result_file_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
+			;;
+		s)
+			if (( ! $SERVIENT_sol_dir_is_set ))
+			then
+				echo "-s was triggered, Parameter: $OPTARG is the solution directory/file" >&2
+				SERVIENT_sol_dir_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		u)
-			echo "-f was triggered, $OPTARG file in each directory will be queried for user info" >&2
+			if (( ! $SERVIENT_uinfo_string_is_set ))
+			then
+				echo "-u was triggered, $OPTARG string will be executed on the contents of file given in -f argument" >&2
+				SERVIENT_uinfo_string_is_set=1
+			else
+				echo "More than one instance of $opt given during invocation" >&2
+				exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
+			fi
 			;;
 		\?)
 			echo "Invalid option: -$OPTARG" >&2
-			exit 1 ## TODO: Put proper exit codes
+			exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
 			;;
 		:)
 			echo "Option -$OPTARG requires an argument." >&2
-			exit 1 ## TODO: Put proper exit codes
+			exit $SERVIENT_EXIT_ERROR_SCRIPT_CONFIG
 			;;
 	esac
 done
@@ -178,6 +280,7 @@ do
 	DIR=`echo $DIR| sed 's/^\.\///'`;	
 	if ( [ "$DIR" != "$REFERENCE_SCRIPTS_DIR_NAME" ] && [ "$DIR" != "$META_DIR_NAME" ] && [ "$DIR" != "." ] && [ "$DIR" != ".." ] ) 
 	then
+		# XXX $META_DIR_NAME check needs to be there. It is not always guarenteed to be inside reference script directory.
 		MAGIC_STRING="" ## TODO: see TODO
 		SCORE=0
 		USER_ID=$(get_user_id)
